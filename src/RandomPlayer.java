d120bc023d4119e0ab85e364d24d5b2dd09ce184
vaibhav <vaibhav46@outlook.com>
2019-10-17 16:26:51 -0600

    Random player WIP - 1

--- src/RandomPlayer.java
+++ src/RandomPlayer.java
@@ -1,6 +1,18 @@
+import java.util.Random;
+
 public class RandomPlayer extends Player{
+    RandomGenerator randomGenerator;
+
+
     public RandomPlayer(String name, char mark) {
         super(name, mark);
+        this.randomGenerator = new RandomGenerator();
     }
 
+    @Override
+    public void makeMove() {
+        //super.makeMove();
+        super.getBoard().addMark(this.randomGenerator.discrete(0, 3), this.randomGenerator.discrete(0, 3), super.getMark());
+        super.setBoard(super.getBoard());
+    }
 }
